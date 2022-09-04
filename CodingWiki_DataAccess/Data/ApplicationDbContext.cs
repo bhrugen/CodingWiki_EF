ed6c4d7c57846d7bf5338898d6b7c517ccae29bf
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-04 13:55:34 -0500

    Section 2 - Seed Data using Migration

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -21,6 +21,19 @@ namespace CodingWiki_DataAccess.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Book>().Property(u => u.Price).HasPrecision(10, 5);
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book { BookId = 1, Title = "Spider without Duty", ISBN = "123B12", Price = 10.99m },
+                new Book { BookId = 2, Title = "Fortune of time", ISBN = "12123B12", Price = 11.99m }
+            );
+
+            var bookList = new Book[]
+            {
+                new Book { BookId = 3, Title = "Fake Sunday", ISBN = "77652", Price = 20.99m },
+                new Book { BookId = 4, Title = "Cookie Jar", ISBN = "CC12B12", Price = 25.99m },
+                new Book { BookId = 5, Title = "Cloudy Forest", ISBN = "90392B33", Price = 40.99m }
+            };
+            modelBuilder.Entity<Book>().HasData(bookList);
         }
 
     }
---
+++ CodingWiki_DataAccess/Migrations/20220904185429_seedBookTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedBookTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "BookId", "ISBN", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "123B12", 10.99m, "Spider without Duty" },
+                    { 2, "12123B12", 11.99m, "Fortune of time" },
+                    { 3, "77652", 20.99m, "Fake Sunday" },
+                    { 4, "CC12B12", 25.99m, "Cookie Jar" },
+                    { 5, "90392B33", 40.99m, "Cloudy Forest" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 5);
+        }
+    }
+}
