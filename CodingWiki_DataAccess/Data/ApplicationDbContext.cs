1ed4ec97eb3c8f38aa1fd2563020117881ed0800
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-04 09:38:53 -0500

    Section 2 - Create ApplicationDbContext and Book Model

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ CodingWiki_Model/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_Model.Models
+{
+    public class Book
+    {
+        public int IDBook { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public double Price { get; set; }
+    }
+}
