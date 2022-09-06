79012d25722ce6a467d2e56ce40cf3045def54fe
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-06 12:00:18 -0500

    Section 3 - Max Length and Not Mapped

---
+++ CodingWiki_DataAccess/Migrations/20220906165909_maxLengthAndNotMappedInBooksTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class maxLengthAndNotMappedInBooksTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
--- CodingWiki_Model/Models/Book.cs
+++ CodingWiki_Model/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,11 @@ namespace CodingWiki_Model.Models
        // [Key]
         public int BookId { get; set; }
         public string Title { get; set; }
+        [MaxLength(20)]
+        [Required]
         public string ISBN { get; set; }
         public decimal Price { get; set; }
+        [NotMapped]
+        public string PriceRange { get; set; }
     }
 }
