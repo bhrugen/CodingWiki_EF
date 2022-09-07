a3051ef167cc22eb405f67f8e05fcc0ab3fd4227
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-07 11:00:26 -0500

    Section 7 - Seed Category and Logging in EF Core

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -18,74 +18,74 @@ Console.WriteLine("Hello, World!");
 //GetAllBooks();
 //GetBook();
 //UpdateBook();
-DeleteBook();
+//DeleteBook();
 
-async void DeleteBook()
-{
-    using var context = new ApplicationDbContext();
-    var book = await context.Books.FindAsync(1003);
-    context.Books.Remove(book);
-    await context.SaveChangesAsync();
-}
+//async void DeleteBook()
+//{
+//    using var context = new ApplicationDbContext();
+//    var book = await context.Books.FindAsync(1003);
+//    context.Books.Remove(book);
+//    await context.SaveChangesAsync();
+//}
 
-async void UpdateBook()
-{
-    try
-    {
-        using var context = new ApplicationDbContext();
-        var books = await context.Books.Where(u=>u.Publisher_Id==1).ToListAsync();
-        //Console.WriteLine(book.Title + " - " + book.ISBN);
-        foreach(var book in books)
-        {
-            book.Price = 55.55m;
-        }
-        await context.SaveChangesAsync();
-    }
-    catch (Exception e)
-    {
+//async void UpdateBook()
+//{
+//    try
+//    {
+//        using var context = new ApplicationDbContext();
+//        var books = await context.Books.Where(u=>u.Publisher_Id==1).ToListAsync();
+//        //Console.WriteLine(book.Title + " - " + book.ISBN);
+//        foreach(var book in books)
+//        {
+//            book.Price = 55.55m;
+//        }
+//        await context.SaveChangesAsync();
+//    }
+//    catch (Exception e)
+//    {
 
-    }
-}
+//    }
+//}
 
-async void GetBook()
-{
-    try
-    {
-        using var context = new ApplicationDbContext();
-        var books = await context.Books.Skip(0).Take(2).ToListAsync();
-        //Console.WriteLine(book.Title + " - " + book.ISBN);
-        foreach (var book in books)
-        {
-            Console.WriteLine(book.Title + " - " + book.ISBN);
-        }
+//async void GetBook()
+//{
+//    try
+//    {
+//        using var context = new ApplicationDbContext();
+//        var books = await context.Books.Skip(0).Take(2).ToListAsync();
+//        //Console.WriteLine(book.Title + " - " + book.ISBN);
+//        foreach (var book in books)
+//        {
+//            Console.WriteLine(book.Title + " - " + book.ISBN);
+//        }
 
-        books = await context.Books.Skip(4).Take(1).ToListAsync();
-        foreach (var book in books)
-        {
-            Console.WriteLine(book.Title + " - " + book.ISBN);
-        }
-    }
-    catch (Exception e)
-    {
+//        books = await context.Books.Skip(4).Take(1).ToListAsync();
+//        foreach (var book in books)
+//        {
+//            Console.WriteLine(book.Title + " - " + book.ISBN);
+//        }
+//    }
+//    catch (Exception e)
+//    {
 
-    }
-}
+//    }
+//}
 
-void GetAllBooks()
-{
-    using var context = new ApplicationDbContext();
-    var books = context.Books.ToList();
-    foreach (var book in books)
-    {
-        Console.WriteLine(book.Title + " - " + book.ISBN);
-    }
-}
+//void GetAllBooks()
+//{
+//    using var context = new ApplicationDbContext();
+//    var books = context.Books.ToList();
+//    foreach (var book in books)
+//    {
+//        Console.WriteLine(book.Title + " - " + book.ISBN);
+//    }
+//}
 
 
-async void AddBook()
-{
-    Book book = new() { Title = "New EF Core Book", ISBN = "9999", Price = 10.93m, Publisher_Id=1 };
-    using var context = new ApplicationDbContext();
-    var books = await context.Books.AddAsync(book);
-    await context.SaveChangesAsync();
-}
+//async void AddBook()
+//{
+//    Book book = new() { Title = "New EF Core Book", ISBN = "9999", Price = 10.93m, Publisher_Id=1 };
+//    using var context = new ApplicationDbContext();
+//    var books = await context.Books.AddAsync(book);
+//    await context.SaveChangesAsync();
+//}
---
+++ CodingWiki_DataAccess/Migrations/20220907155356_seedCategory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories VALUES ('Cat 1')");
+            migrationBuilder.Sql("INSERT INTO Categories VALUES ('Cat 2')");
+            migrationBuilder.Sql("INSERT INTO Categories VALUES ('Cat 3')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
