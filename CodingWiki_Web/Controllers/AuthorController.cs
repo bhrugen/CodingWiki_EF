f62a084316572182e8fd35164340dd309d62b734
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-07 15:50:55 -0500

    Section 7 - Setup Author and Publisher Views

---
+++ CodingWiki_Web/Controllers/AuthorController.cs
@@ -0,0 +1,39 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_Web.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AuthorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Category obj)
+        {
+
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ CodingWiki_Web/Controllers/PublisherController.cs
@@ -0,0 +1,41 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_Web.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public PublisherController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Category obj)
+        {
+            
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+           
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
