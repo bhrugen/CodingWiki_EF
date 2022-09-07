2f3f642e3bfaece9504881b56e4fa069b1e112de
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-07 07:22:25 -0500

    Section 6 - Get All Books

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -13,3 +13,14 @@ Console.WriteLine("Hello, World!");
 //    }
 //}
 
+GetAllBooks();
+
+void GetAllBooks()
+{
+    using var context = new ApplicationDbContext();
+    var books = context.Books.ToList();
+    foreach (var book in books)
+    {
+        Console.WriteLine(book.Title+ " - " + book.ISBN); 
+    }
+}
