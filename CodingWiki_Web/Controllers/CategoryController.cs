87b9a30085bac5fbab95e151418cb90102323e6e
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-07 12:47:31 -0500

    Section 7 - Retrive using EF Core

---
+++ CodingWiki_Web/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objList = _db.Categories.ToList();
+            return View(objList);
+        }
+    }
+}
