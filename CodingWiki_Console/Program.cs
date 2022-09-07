7f5a8f142c456ea2df384a9e38e5aa761e7cdbc3
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-07 07:15:13 -0500

    Section 6 - Database helper methods

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CodingWiki_DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+//using(ApplicationDbContext context = new())
+//{
+//    context.Database.EnsureCreated();
+//    if (context.Database.GetPendingMigrations().Count() > 0) 
+//    {
+//        context.Database.Migrate(); 
+//    }
+//}
+
