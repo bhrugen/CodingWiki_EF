c49eae023fa926658d20d173b4634303c6a1a907
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-07 10:51:54 -0500

    Section 7 - Setup EF Core in Web Project

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -27,11 +27,13 @@ namespace CodingWiki_DataAccess.Data
         public DbSet<Fluent_BookAuthorMap> Fluent_BookAuthorMaps { get; set; }
 
 
-
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer("Server=(LocalDb)\\MSSQLLocalDB;Database=CodingWiki;TrustServerCertificate=True;Trusted_Connection=True;")
-                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name },LogLevel.Information);
+        //    options.UseSqlServer("Server=(LocalDb)\\MSSQLLocalDB;Database=CodingWiki;TrustServerCertificate=True;Trusted_Connection=True;")
+          //      .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name },LogLevel.Information);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- CodingWiki_Web/Program.cs
+++ CodingWiki_Web/Program.cs
@@ -1,8 +1,13 @@
+using CodingWiki_DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
