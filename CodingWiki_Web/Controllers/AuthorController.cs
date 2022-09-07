82de546c3863e1259f4e9b67882c96236e2a29b4
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-07 15:56:50 -0500

    Section 7 - Assignment 5 Solution - Publisher and Author CRUD

--- CodingWiki_Web/Controllers/AuthorController.cs
+++ CodingWiki_Web/Controllers/AuthorController.cs
@@ -14,25 +14,62 @@ namespace CodingWiki_Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<Author> objList = _db.Authors.ToList();
+            return View(objList);
         }
 
         public IActionResult Upsert(int? id)
         {
-            return View();
+            Author obj = new();
+            if (id == null || id == 0)
+            {
+                //create
+                return View(obj);
+            }
+            //edit
+            obj = _db.Authors.FirstOrDefault(u => u.Author_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Upsert(Category obj)
+        public async Task<IActionResult> Upsert(Author obj)
         {
 
-            return View();
+            if (ModelState.IsValid)
+            {
+                if (obj.Author_Id == 0)
+                {
+                    //create
+                    await _db.Authors.AddAsync(obj);
+                }
+                else
+                {
+                    //update
+                    _db.Authors.Update(obj);
+                }
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
         }
 
         public async Task<IActionResult> Delete(int id)
         {
 
+            Author obj = new();
+            obj = _db.Authors.FirstOrDefault(u => u.Author_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Authors.Remove(obj);
+            await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
     }
--- CodingWiki_Web/Controllers/PublisherController.cs
+++ CodingWiki_Web/Controllers/PublisherController.cs
@@ -14,25 +14,62 @@ namespace CodingWiki_Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<Publisher> objList = _db.Publishers.ToList();
+            return View(objList);
         }
 
         public IActionResult Upsert(int? id)
         {
-            return View();
+            Publisher obj = new();
+            if (id == null || id == 0)
+            {
+                //create
+                return View(obj);
+            }
+            //edit
+            obj = _db.Publishers.FirstOrDefault(u => u.Publisher_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Upsert(Category obj)
+        public async Task<IActionResult> Upsert(Publisher obj)
         {
-            
-            return View();
+
+            if (ModelState.IsValid)
+            {
+                if (obj.Publisher_Id == 0)
+                {
+                    //create
+                    await _db.Publishers.AddAsync(obj);
+                }
+                else
+                {
+                    //update
+                    _db.Publishers.Update(obj);
+                }
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
         }
 
         public async Task<IActionResult> Delete(int id)
         {
-           
+
+            Publisher obj = new();
+            obj = _db.Publishers.FirstOrDefault(u => u.Publisher_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Publishers.Remove(obj);
+            await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
