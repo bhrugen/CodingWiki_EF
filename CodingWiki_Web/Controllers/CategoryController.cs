af41b0c4e38120bc7b44391cc6b58aa1f253fa1a
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-07 13:17:57 -0500

    Section 7 - Delete Category

--- CodingWiki_Web/Controllers/CategoryController.cs
+++ CodingWiki_Web/Controllers/CategoryController.cs
@@ -56,5 +56,19 @@ namespace CodingWiki_Web.Controllers
             }
             return View(obj);
         }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            Category obj = new();
+            obj = _db.Categories.First(u => u.CategoryId == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
