f4e1e58b7453c16d9a3215ae02b2a39dc30cccb0
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-07 08:55:04 -0500

    Section 6 - Contains, Like and Aggregation

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -23,12 +23,12 @@ void GetBook()
     try
     {
         using var context = new ApplicationDbContext();
-        var book = context.Books.SingleOrDefault(u=>u.Publisher_Id== 66);
-        Console.WriteLine(book.Title + " - " + book.ISBN);
-        //foreach (var book in books)
-        //{
-        //    Console.WriteLine(book.Title + " - " + book.ISBN);
-        //}
+        var books = context.Books.Where(u=>EF.Functions.Like(u.ISBN,"12%"));
+        //Console.WriteLine(book.Title + " - " + book.ISBN);
+        foreach (var book in books)
+        {
+            Console.WriteLine(book.Title + " - " + book.ISBN);
+        }
     }
     catch (Exception e)
     {
