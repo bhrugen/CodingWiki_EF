98f263fe9eea7ee349374e80b73a63c707ed9235
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-09-07 10:29:21 -0500

    Section 6 - Async Methods

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -20,26 +20,26 @@ Console.WriteLine("Hello, World!");
 //UpdateBook();
 DeleteBook();
 
-void DeleteBook()
+async void DeleteBook()
 {
     using var context = new ApplicationDbContext();
-    var book = context.Books.Find(1003);
+    var book = await context.Books.FindAsync(1003);
     context.Books.Remove(book);
-    context.SaveChanges();
+    await context.SaveChangesAsync();
 }
 
-void UpdateBook()
+async void UpdateBook()
 {
     try
     {
         using var context = new ApplicationDbContext();
-        var books = context.Books.Where(u=>u.Publisher_Id==1);
+        var books = await context.Books.Where(u=>u.Publisher_Id==1).ToListAsync();
         //Console.WriteLine(book.Title + " - " + book.ISBN);
         foreach(var book in books)
         {
             book.Price = 55.55m;
         }
-        context.SaveChanges();
+        await context.SaveChangesAsync();
     }
     catch (Exception e)
     {
@@ -47,19 +47,19 @@ void UpdateBook()
     }
 }
 
-void GetBook()
+async void GetBook()
 {
     try
     {
         using var context = new ApplicationDbContext();
-        var books = context.Books.Skip(0).Take(2);
+        var books = await context.Books.Skip(0).Take(2).ToListAsync();
         //Console.WriteLine(book.Title + " - " + book.ISBN);
         foreach (var book in books)
         {
             Console.WriteLine(book.Title + " - " + book.ISBN);
         }
 
-        books = context.Books.Skip(4).Take(1);
+        books = await context.Books.Skip(4).Take(1).ToListAsync();
         foreach (var book in books)
         {
             Console.WriteLine(book.Title + " - " + book.ISBN);
@@ -82,10 +82,10 @@ void GetAllBooks()
 }
 
 
-void AddBook()
+async void AddBook()
 {
     Book book = new() { Title = "New EF Core Book", ISBN = "9999", Price = 10.93m, Publisher_Id=1 };
     using var context = new ApplicationDbContext();
-    var books = context.Books.Add(book);
-    context.SaveChanges();
+    var books = await context.Books.AddAsync(book);
+    await context.SaveChangesAsync();
 }
